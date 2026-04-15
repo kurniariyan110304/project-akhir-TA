7adcbeec26ad393147e0b77e5474d5b632e2c22e
akhmad arip <akhmadarip86@gmail.com>
2026-04-15 13:00:53 +0700

    update redirect

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,8 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
- return redirect()->route('login');
+    return redirect('/mahasiswa/login');
 });
-
 // Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 // Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 // Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
